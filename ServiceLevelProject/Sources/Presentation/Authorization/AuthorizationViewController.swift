85c14bb74f34ab594145231bdea212d49432ebd4
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-10-28 18:28:42 +0900

    feat: AuthorizationView UI 작업

--- ServiceLevelProject/Sources/Presentation/Authorization/AuthorizationViewController.swift
+++ ServiceLevelProject/Sources/Presentation/Authorization/AuthorizationViewController.swift
@@ -6,17 +6,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class AuthorizationViewController: UIViewController {
+final class AuthorizationViewController: UIViewController {
 
     private let authorizationView = AuthorizationView()
-    
+    private let disposeBag = DisposeBag()
+
     override func loadView() {
         view = authorizationView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bind()
+    }
+}
+
+extension AuthorizationViewController {
+    private func bind() {
+        authorizationView.appleButton.rx.tap
+            .bind(with: self) { owner, _ in
+                print("Apple Button Tap")
+            }
+            .disposed(by: disposeBag)
+        
+        authorizationView.kakaoButton.rx.tap
+            .bind(with: self) { owner, _ in
+                print("Kakao Button Tap")
+            }
+            .disposed(by: disposeBag)
+        
+        authorizationView.emailButton.rx.tap
+            .bind(with: self) { owner, _ in
+                print("Email Button Tap")
+            }
+            .disposed(by: disposeBag)
+        
+        authorizationView.signupButton.rx.tap
+            .bind(with: self) { owner, _ in
+                print("Signup Button Tap")
+            }
+            .disposed(by: disposeBag)
     }
 }
 
