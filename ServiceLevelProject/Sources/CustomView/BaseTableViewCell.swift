92696a22113c41e9c23b970a653fcc67b57703a6
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-11-05 19:59:04 +0900

    fix: BaseTableView init 수정

--- ServiceLevelProject/Sources/CustomView/BaseTableViewCell.swift
+++ ServiceLevelProject/Sources/CustomView/BaseTableViewCell.swift
@@ -8,14 +8,14 @@
 import UIKit
 
 class BaseTableViewCell: UITableViewCell {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         addSubviews()
         setConstraints()
         configureUI()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
