9b8210ed65cf838c011757331b477837eb0bfd1b
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-11-08 19:25:24 +0900

    feat: ChattingView textview 관련 UI 구현

--- ServiceLevelProject/Sources/Presentation/Chatting/ChattingView.swift
+++ ServiceLevelProject/Sources/Presentation/Chatting/ChattingView.swift
@@ -17,10 +17,36 @@ final class ChattingView: BaseView {
     }
     
     private let buttonView = UIView().then {
-        $0.backgroundColor = .lightGray
+        $0.backgroundColor = .backgroundPrimary
+        $0.layer.cornerRadius = 8
+    }
+    
+    private let plusButton = UIButton().then {
+        let image = UIImage(resource: .plusPhoto)
+        $0.setImage(image, for: .normal)
+    }
+    
+    private lazy var chatTextField = UITextView().then {
+        $0.font = .body
+        $0.text = "메시지를 입력하세요"
+        $0.textColor = .textSecondary
+        $0.backgroundColor = .backgroundPrimary
+        $0.textContainerInset = .zero
+        $0.isScrollEnabled = false
+        $0.delegate = self
+    }
+    
+    private let sendButton = UIButton().then {
+        let image = UIImage(resource: .send)
+        $0.setImage(image, for: .normal)
     }
     
     override func addSubviews() {
+        buttonView.addSubviews([
+            plusButton,
+            chatTextField,
+            sendButton,
+        ])
         addSubviews([
             chattingTableView,
             buttonView
@@ -29,17 +55,73 @@ final class ChattingView: BaseView {
     
     override func setConstraints() {
         let safe = safeAreaLayoutGuide
-        
+
         buttonView.snp.makeConstraints {
             $0.height.equalTo(38)
             $0.horizontalEdges.equalTo(safe).inset(16)
             $0.bottom.equalTo(safe).inset(12)
         }
         
+        plusButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(12)
+            $0.width.equalTo(22)
+            $0.bottom.equalToSuperview().inset(9)
+        }
+        
+        sendButton.snp.makeConstraints {
+            $0.width.equalTo(24)
+            $0.bottom.equalToSuperview().inset(7)
+            $0.trailing.equalToSuperview().inset(12)
+        }
+        
+        chatTextField.snp.makeConstraints {
+            $0.leading.equalTo(plusButton.snp.trailing).offset(8)
+            $0.trailing.equalTo(sendButton.snp.leading).offset(8)
+            $0.height.equalTo(18)
+            $0.verticalEdges.equalToSuperview().inset(10)
+        }
+        
         chattingTableView.snp.makeConstraints {
             $0.top.equalTo(safe).offset(16)
             $0.horizontalEdges.equalTo(safe)
             $0.bottom.equalTo(buttonView.snp.top)
+        }
+    }
+}
+
+extension ChattingView: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.textSecondary {
+            textView.text = nil
+            textView.textColor = UIColor.textPrimary
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "메세지를 입력하세요"
+            textView.textColor = UIColor.textSecondary
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let maxHeight: CGFloat = 18 * 3
+        let fixedWidth = textView.frame.size.width
+        let sizeThatFits = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+        var newHeight = sizeThatFits.height
+        if newHeight > maxHeight {
+            newHeight = maxHeight
+            textView.isScrollEnabled = true
+        } else {
+            textView.isScrollEnabled = false
+        }
+
+        textView.snp.updateConstraints {
+            $0.height.equalTo(newHeight)
+        }
+
+        buttonView.snp.updateConstraints {
+            $0.height.equalTo(newHeight + 20)
         }
     }
 }
