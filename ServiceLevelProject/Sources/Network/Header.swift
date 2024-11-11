c6241b46535f15708c4ad10c5ec79cdd77f01f3c
yeggrrr <yjkim981103@gmail.com>
2024-11-11 22:27:27 +0900

    feat: 네트워크 통신 Router 구현

---
+++ ServiceLevelProject/Sources/Network/Header.swift
@@ -0,0 +1,17 @@
+//
+//  Header.swift
+//  ServiceLevelProject
+//
+//  Created by YJ on 11/11/24.
+//
+
+import Foundation
+
+enum Header: String {
+    case authorization = "Authorization"
+    case sesacKey = "SesacKey"
+    case contentType = "Content-Type"
+    case accept = "accept"
+    case json = "application/json"
+    case mutipart = "multipart/form-data"
+}
---
+++ ServiceLevelProject/Sources/Network/TargetType.swift
@@ -0,0 +1,34 @@
+//
+//  TargetType.swift
+//  ServiceLevelProject
+//
+//  Created by YJ on 11/11/24.
+//
+
+import Foundation
+import Alamofire
+
+protocol TargetType: URLRequestConvertible {
+    var baseURL: String { get }
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var header: [String : String] { get }
+    var parameters: [String : String]? { get }
+    var queryItems: [URLQueryItem]? { get }
+    var body: Data? { get }
+}
+
+extension TargetType {
+    func asURLRequest() throws -> URLRequest {
+        let url = try baseURL.asURL()
+        let urlString = url.appendingPathComponent(path).absoluteString
+        guard var urlComponents = URLComponents(string: urlString) else { return URLRequest(url: url) }
+        urlComponents.queryItems = queryItems
+        
+        guard let requestURL = urlComponents.url else { return URLRequest(url: url) }
+        var request = try URLRequest(url: requestURL, method: method)
+        request.allHTTPHeaderFields = header
+        request.httpBody = body
+        return request
+    }
+}
