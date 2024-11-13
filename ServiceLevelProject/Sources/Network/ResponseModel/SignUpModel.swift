e8fb36b242ba28c716caca505e4c9e5b8d9135b0
yeggrrr <yjkim981103@gmail.com>
2024-11-14 04:08:24 +0900

    fix: SignUpModel 타입 수정

--- ServiceLevelProject/Sources/Network/ResponseModel/SignUpModel.swift
+++ ServiceLevelProject/Sources/Network/ResponseModel/SignUpModel.swift
@@ -11,11 +11,11 @@ struct SignUpModel: Decodable {
     let userID: String
     let email: String
     let nickname: String
-    let profileImage: String
-    let phone: String?
+    let profileImage: String?
+    let phone: String
     let provider: String?
     let createdAt: String
-    let token: [Token]
+    let token: Token
     
     enum CodingKeys: String, CodingKey {
         case userID = "user_id"
