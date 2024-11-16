5cfa0e2280e90ecdd68a0446998a2c7a6d659bec
yeggrrr <yjkim981103@gmail.com>
2024-11-16 21:54:09 +0900

    chore: APIManager 변경으로 인한 코드 수정

--- ServiceLevelProject/Sources/Presentation/Login/LoginViewModel.swift
+++ ServiceLevelProject/Sources/Presentation/Login/LoginViewModel.swift
@@ -57,7 +57,7 @@ final class LoginViewModel: ViewModelBindable {
         // 유효성 검증 통과 후, 로그인 네트워크 통신
         input.isLoggingIn
             .withLatestFrom(Observable.combineLatest(input.emailText, input.passwordText))
-            .flatMapLatest { email, password -> Single<Result<UserModel, ErrorCode>> in
+            .flatMapLatest { email, password -> Single<Result<UserModel, ErrorModel>> in
                 let query = LoginQuery(email: email, password: password, deviceToken: UserDefaultManager.fcmToken ?? "")
                 return APIManager.shared.callRequest(api: UserRouter.login(query: query), type: UserModel.self)
             }
