09af3ed45c47366cc7c47aa51e52e55f233ed6ba
yeggrrr <yjkim981103@gmail.com>
2024-11-27 01:08:13 +0900

    chore: TabVC viewDidLoad 코드 loadAllTabViews 메서드로 분리

--- ServiceLevelProject/Sources/CustomView/TabbarViewController.swift
+++ ServiceLevelProject/Sources/CustomView/TabbarViewController.swift
@@ -10,15 +10,9 @@ import UIKit
 final class TabbarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureTabbar()
         
-        viewControllers?.forEach {
-            if let nav = $0 as? UINavigationController {
-                let _ = nav.topViewController?.view
-            } else {
-                let _ = $0.view
-            }
-        }
+        configureTabbar()
+        loadAllTabViews()
     }
 }
 
@@ -79,6 +73,18 @@ extension TabbarViewController {
         
         var tabbarItem: UITabBarItem {
             return UITabBarItem(title: title, image: image, selectedImage: selectedImage)
+        }
+    }
+}
+
+extension TabbarViewController {
+    private func loadAllTabViews() {
+        viewControllers?.forEach {
+            if let nav = $0 as? UINavigationController {
+                let _ = nav.topViewController?.view
+            } else {
+                let _ = $0.view
+            }
         }
     }
 }
