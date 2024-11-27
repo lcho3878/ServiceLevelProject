8726d9fc818fcb566fb56ac74d56cc0569279a0b
lcho3878 <74814587+lcho3878@users.noreply.github.com>
2024-11-28 03:42:47 +0900

    feat: 채팅 내역 조회, 보내기 case 추가

--- ServiceLevelProject/Sources/Network/DMRouter.swift
+++ ServiceLevelProject/Sources/Network/DMRouter.swift
@@ -11,6 +11,8 @@ import Alamofire
 enum DMRouter {
     case dmList(workspaceID: String)
     case unreadCount(workspaceID: String, roomID: String, after: String)
+    case chattingList(workspaceID: String, roomID: String, after: String)
+    case sendChatting(workspaceID: String, roomID: String, query: ChattingQuery)
 }
 
 extension DMRouter : TargetType {
@@ -20,8 +22,10 @@ extension DMRouter : TargetType {
     
     var method: HTTPMethod {
         switch self {
-        case .dmList, .unreadCount:
+        case .dmList, .unreadCount, .chattingList:
             return .get
+        case .sendChatting:
+            return .post
         }
     }
     
@@ -31,13 +35,17 @@ extension DMRouter : TargetType {
             return "/workspaces/\(workspaceID)/dms"
         case let .unreadCount(workspaceID, roomID, _):
             return "/workspaces/\(workspaceID)/dms/\(roomID)/unreads"
+        case let .chattingList(workspaceID, roomID, _):
+            return "/workspaces/\(workspaceID)/dms/\(roomID)/chats"
+        case let .sendChatting(workspaceID, roomID, _):
+            return "/workspaces/\(workspaceID)/dms/\(roomID)/chats"
         }
     }
     
     var header: [String : String] {
         switch self {
-        case .dmList, .unreadCount:
-            return [
+        case .dmList, .unreadCount, .chattingList, .sendChatting:
+             [
                 Header.accept.rawValue: Header.json.rawValue,
                 Header.sesacKey.rawValue: Key.sesacKey,
                 Header.authorization.rawValue: UserDefaultManager.accessToken ?? ""
@@ -51,6 +59,10 @@ extension DMRouter : TargetType {
             return [
                 "after": after
             ]
+        case let .chattingList(_, _, after):
+            return [
+                "cursor_date": after
+            ]
         default:
             return nil
         }
@@ -58,7 +70,7 @@ extension DMRouter : TargetType {
     
     var queryItems: [URLQueryItem]? {
         switch self {
-        case .unreadCount:
+        case .unreadCount, .chattingList:
             return parameters?.map {
                 URLQueryItem(name: $0.key, value: $0.value)
             }
@@ -72,6 +84,18 @@ extension DMRouter : TargetType {
     }
     
     var multipartFormData: MultipartFormData? {
-        return nil
+        switch self {
+        case let .sendChatting(_, _, query):
+            let multipart = MultipartFormData()
+            let content = query.content.data(using: .utf8) ?? Data()
+            multipart.append(content, withName: "content")
+            if query.files.isEmpty { return multipart }
+            for (i, file) in query.files.enumerated() {
+                guard let file else { continue }
+                multipart.append(file, withName: "files", fileName: "\(i).jpeg", mimeType: "image/jpeg")
+            }
+            return multipart
+        default: return nil
+        }
     }
 }
